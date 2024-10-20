5b45f75ae2290b11ecf904d33f237a61cdff7bef
Ahmet Rauf Yavuz <ahmetraufyavuz@hotmail.com>
2024-10-20 16:29:15 +0300

    Ödev tamamlandı
    Bilgisayar türkçe olduğundan I harfinin küçüğünü ı olarak alıyor o yüzden testlerin bir tanesinden kalıyor. başka bir problem yok.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,49 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
     }
+
+
+    public static boolean checkForPalindrome(String string){
+        String plainText = string.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        LinkedList<Character> list = new LinkedList<>();
+        for (char c : plainText.toCharArray()){
+            list.add(c);
+        }
+
+        while(list.size()>1){
+            if (!list.pollFirst().equals(list.pollLast())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public  static String convertDecimalToBinary(int x){
+
+        LinkedList<Integer> result = new LinkedList<>();
+
+        while (x > 0 ){
+            int reminder = x % 2 ;
+            result.addFirst(reminder);
+            x = x/2;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int y : result){
+            stringBuilder.append(y);
+        }
+        return stringBuilder.toString();
+
+    }
+
+
 
 }
